0b2defc6618f1ba8511de550835f7cd239dd611b
Milad <miladxgh@outlook.com>
2021-12-15 20:36:53 +0330

    Add coinHeader component

---
+++ src/CoinHeader.jsx
@@ -0,0 +1,14 @@
+const CoinHeader = () => {
+  return (
+    <div className="coins-header">
+      <span style={{width: "55px"}}>Rank</span>
+      <span style={{width: "50px"}}></span>
+      <span style={{width: "200px"}}>Coin</span>
+      <span style={{width: "100px"}}>Price</span>
+      <span style={{width: "50px"}}>24h</span>
+      <span style={{width: "110px"}}>Market cap</span>
+    </div>
+  );
+}
+
+export default CoinHeader
--- src/Home.jsx
+++ src/Home.jsx
@@ -2,21 +2,24 @@ import { useState, useEffect } from "react"
 import { Link } from "react-router-dom"
 import axios from "axios"
 import Coin from "./Coin"
+import CoinHeader from './CoinHeader'
 
 const Home = () => {
   const [coins, setCoins] = useState([]);
 
   useEffect(() => {
     axios
-      .get("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1&sparkline=false")
+      .get("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false")
       .then((res) => {
         setCoins(res.data);
+        console.log(coins);
       });
   }, []);
 
   return (
     <div className="page-wrapper">
       <div className="coins-container">
+         <CoinHeader />
         {
           coins.map((coin) => (
             <Link to={`/coins/${coin.id}`} key={coin.id}>
