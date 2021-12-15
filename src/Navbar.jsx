6f2ef70820b3ac6f29eb8774b59ab6f089035bc5
Milad <miladxgh@outlook.com>
2021-12-15 22:38:32 +0330

    Add navbar logo and style

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,5 +1,5 @@
 import { IoSearch } from "react-icons/io5";
-
+import logo from './img/logo.png'
 
 const Navigation = () => {
   return (
@@ -7,30 +7,31 @@ const Navigation = () => {
       <div className="page-wrapper">
         <nav>
           <div className="nav-logo">
-            <span>coingecko</span>
+            <a href="/"><img src={logo} alt="logo" /></a>
+            <span>Tetra</span>
           </div>
 
-            <div className="search-container">
-              <input
-                className="search-input"
-                type="seach"
-                placeholder="search..."
-              />
+          <div className="search-container">
+            <input
+              className="search-input"
+              type="seach"
+              placeholder="search..."
+            />
 
-              <div className="search-icon">
-                <IoSearch />
-              </div>
+            <div className="search-icon">
+              <IoSearch />
             </div>
+          </div>
           
           <ul className="nav-list">
             <li>
-              <a href="#">Price</a>
+              <a className="nav-link" href="#">Price</a>
             </li>
             <li>
-              <a href="#">Market</a>
+              <a className="nav-link" href="#">Market</a>
             </li>
             <li>
-              <a href="#">About</a>
+              <a className="nav-link" href="#">About</a>
             </li>
           </ul>
         </nav>
