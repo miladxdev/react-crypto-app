91069a1e3df2877f98d753ded67bd04742078dff
Milad <miladxgh@outlook.com>
2021-12-15 20:39:00 +0330

    Add class names for coin component items

--- src/Coin.jsx
+++ src/Coin.jsx
@@ -1,3 +1,6 @@
+import { BsStar } from 'react-icons/bs'
+import { BsFillStarFill } from 'react-icons/bs'
+
 const Coin = ({ coin }) => {
   const priceChangePositiveStyle = {
     background: "lightgreen",
@@ -10,29 +13,33 @@ const Coin = ({ coin }) => {
 
   return (
     <div className="coin">
+      <div className="coin-rank">
+        {coin.market_cap_rank}
+        <div className="coin-favorite"><BsStar /></div>
+      </div>
+
       <div className="coin-image-wrapper">
-        <span className="coin-rank">{coin.market_cap_rank}</span>
         <img src={coin.image} alt="coin" />
-        <span className="coin-symbol">{coin.symbol}</span>
       </div>
 
-      <h4>{coin.name}</h4>
-
-      <p>{coin.current_price.toFixed(2)}$</p>
-
-      <div>
-        <p className="coin-change-24h"
-          style={
-            coin.price_change_percentage_24h > 0
-              ? priceChangePositiveStyle
-              : priceChangeNegativeStyle
-          }
-        >
-          {Math.round(coin.price_change_percentage_24h)} %
-        </p>
+      <div className="coin-name">
+        <h4>{coin.name}</h4>
+        <p className='coin-symbol'>{coin.symbol}</p>
       </div>
 
-      <p>{(coin.market_cap / 10e6).toFixed(2)} m$</p>
+      <p className="coin-price">{coin.current_price.toFixed(2)} $</p>
+
+      <p className="coin-change-24h"
+        style={
+          coin.price_change_percentage_24h > 0
+            ? priceChangePositiveStyle
+            : priceChangeNegativeStyle
+        }
+      >
+        {Math.round(coin.price_change_percentage_24h)} %
+      </p>
+
+      <p className="coin-market-cap">{(coin.market_cap / 10e6).toFixed(2)} m$</p>
     </div>
   );
 };
